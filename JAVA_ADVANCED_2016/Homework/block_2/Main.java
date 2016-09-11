9584b9558569cd52ea2f35d376e927a2a273c7b1
Stepanov Artem <stepanov.artem.v@gmail.com>
2016-09-11 22:04:28 +0300

    Create class Main in package block_2 in source directory Homework

---
+++ JAVA_ADVANCED_2016/Homework/block_2/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package block_2;
+
+/**
+ * @author stepanov_artem
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Developing...");
+
+	}
+
+}
