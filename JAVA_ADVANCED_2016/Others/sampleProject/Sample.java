1148bb8c724f8dafe1c5ed1c65b8e13fa2263eef
Stepanov Artem <stepanov.artem.v@gmail.com>
2016-09-11 21:49:56 +0300

    Create class Sample to test my project

---
+++ JAVA_ADVANCED_2016/Others/sampleProject/Sample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sampleProject;
+
+/**
+ * @author stepanov_artem
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
