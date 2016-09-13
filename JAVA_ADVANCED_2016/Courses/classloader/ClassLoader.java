6dde0d87caa46fc2c3707a760de24cd7e52303de
Stepanov Artem <stepanov.artem.v@gmail.com>
2016-09-13 19:01:06 +0300

    Courses 13.09.2016

---
+++ JAVA_ADVANCED_2016/Courses/classloader/ClassLoader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package classloader;
+
+/**
+ * @author stepanov_artem
+ *
+ */
+public class ClassLoader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
